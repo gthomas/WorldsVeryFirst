73de19b38067d06ccd6030f15c037858b9d6c500
Grant Thomas <jgranthomas@gmail.com>
2020-07-28 14:50:44 -0700

    add a little x thingy

--- WorldsFirst.e31bb0bc.js
+++ WorldsFirst.e31bb0bc.js
@@ -3737,7 +3737,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60955" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50872" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
